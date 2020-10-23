ff7ef451d9aeecffb635a68aa4df1cf61b30da97
Thomas Diaz <thomydiaz00@gmail.com>
2020-10-22 21:11:33 -0300

    Cambios en pagina para listar productos

--- src/main/java/com/crud/CRUDSpring/repository/PersonaRepository.java
+++ src/main/java/com/crud/CRUDSpring/repository/PersonaRepository.java
@@ -10,13 +10,14 @@ import com.crud.CRUDSpring.model.Persona;
 
 /*
  * JpaRepository extends PagingAndSorting, que extends CrudRepository
- * Entonces JpaRepository provee los metodos relacionados con la persistencia
+ * Entonces JpaRepository provee los metodos relacionados con la persistencia 
  * 	de datos (metodos que permiten manipular los datos de la b.d). Al extender
  *  CrudRepository tambien proporciona los metodos de CRUD
  */
 public interface PersonaRepository extends JpaRepository<Persona, Integer> {
 	   
 	 @Query("SELECT p FROM Persona p WHERE p.name LIKE %?1%"
+			 	+ " OR p.equipo LIKE %?1%"
 	            + " OR p.telefono LIKE %?1%"
 	            + " OR p.id LIKE %?1%")
 	 

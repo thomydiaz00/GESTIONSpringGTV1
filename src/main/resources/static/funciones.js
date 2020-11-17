754695b6041a29cc0193bd4b9d2e9594d71e082b
Thomas Diaz <thomydiaz00@gmail.com>
2020-11-17 12:33:25 -0300

    Agrego sweetalert

--- src/main/resources/static/funciones.js
+++ src/main/resources/static/funciones.js
@@ -1,74 +1,9 @@
- function clearSearch() {
-        window.location.href = '/admin/listar';
-    }
-function clearSearchProductos() {
-        window.location.href = '/admin/listarproductos';
-    }
-    
-function clearSearchUser() {
-        window.location.href = '/user/listar';
-    }
-    
-function clearSearchProductosUser() {
-        window.location.href = '/user/listarproductos';
-    }
-
-function eliminarpersona(id) {
-	console.log(id);
-	swal({
-		  title: "Esta seguro de Eliminar?",
-		  text: "Una vez eliminado no se prodra restablecer!",
-		  icon: "warning",
-		  buttons: true,
-		  dangerMode: true,
-		})
-		.then((OK) => {
-		  if (OK) {
-			  $.ajax({
-				 url:"/admin/delete/"+id,
-				 success: function(res) {
-					console.log(res);
-				},			
-			  });
-		    swal("Registro eliminado satisfactoriamente!", {
-		      icon: "success",
-		    }).then((ok)=>{
-		    	if(ok){
-		    		location.href="/admin/listar";
-		    	}
-		    });
-		  } 
-		});
-		
+ 
+function eliminarclase() {
+		swal("Clase eliminada","Los datos de esta clase fueron eliminados", "success");
+	}
+function eliminarProfesor(){
+			swal("Profesor eliminado", "Los datos del profesor fueron eliminados","success");
 
 }
-
-function eliminarproducto(idproducto) {
-	console.log(idproducto);
-	swal({
-		  title: "Esta seguro de Eliminar?",
-		  text: "Una vez eliminado no se prodra restablecer!",
-		  icon: "warning",
-		  buttons: true,
-		  dangerMode: true,
-		})
-		.then((OK) => {
-		  if (OK) {
-			  $.ajax({
-				 url:"/admin/deleteproducto/"+idproducto,
-				 success: function(res) {
-					console.log(res);
-				},			
-			  });
-		    swal("Registro eliminado satisfactoriamente!", {
-		      icon: "success",
-		    }).then((ok)=>{
-		    	if(ok){
-		    		location.href="/admin/listarproductos";
-		    	}
-		    });
-		  } 
-		});
-		
-
-}
+	

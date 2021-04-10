4cd864c3162343b477558d43d315d6f1d6a73ea2
Thomas Diaz <thomydiaz00@gmail.com>
2021-04-10 10:17:35 -0300

    boton de generar pdf funcionando con redireccionamiento

--- src/main/java/com/crud/CRUDSpring/controller/PdfExportController.java
+++ src/main/java/com/crud/CRUDSpring/controller/PdfExportController.java
@@ -14,6 +14,7 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.crud.CRUDSpring.interfaceService.IfServiceClase;
@@ -22,6 +23,7 @@ import com.crud.CRUDSpring.interfaces.interfaceAsistencia;
 import com.crud.CRUDSpring.model.Asistencia;
 import com.crud.CRUDSpring.model.Clase;
 import com.crud.CRUDSpring.model.Profesor;
+import com.crud.CRUDSpring.util.AsistenciaPdfExporter;
 import com.crud.CRUDSpring.util.ClasePdfExporter;
 import com.crud.CRUDSpring.util.HorariosPdfExporter;
 import com.crud.CRUDSpring.util.ProfesorPdfExporter;
@@ -90,11 +92,11 @@ public class PdfExportController {
 
 	}
 
-	@GetMapping("admin/lista_asistencias/export")
-	public void exportarAsistencias(HttpServletResponse response, @PathParam(value = "mes") int mes, @PathParam(value = "profesor") int profesor, @PathParam(value = "clase") int clase ) throws DocumentException, IOException {
+	@GetMapping("admin/lista_asistencias/export/{profesor}/{clase}/{mes}")
+	public void exportarAsistencias(HttpServletResponse response, @PathVariable(value = "mes") int mes, @PathVariable(value = "profesor") int profesor, @PathVariable(value = "clase") int clase ) throws DocumentException, IOException {
 		System.out.println("el mes actual es:" + mes);
 		System.out.println("el id profesor es:" + profesor);
-		System.out.println("el id profesor es:" + clase);
+		System.out.println("el id clase es:" + clase);
 		Optional<Clase> c = serviceClase.clasePorId(clase);
 		Optional<Profesor> p = serviceProfesor.profesorPorId(profesor);
 		List<Asistencia> asistenciasCompletas = new ArrayList<Asistencia>();
@@ -122,8 +124,7 @@ public class PdfExportController {
 		String headerValue = "attachement;filename=Asistencias.pdf";
 
 		response.setHeader(headerKey, headerValue);
-		List<Clase> listadoClase = serviceClase.listarClase();
-		HorariosPdfExporter exporter = new HorariosPdfExporter(listadoClase);
+		AsistenciaPdfExporter exporter = new AsistenciaPdfExporter(p.get(), c.get(), asistencias, asistenciasCompletas, faltas);
 		exporter.export(response);
 		
 
--- src/main/java/com/crud/CRUDSpring/util/AsistenciaPdfExporter.java
+++ src/main/java/com/crud/CRUDSpring/util/AsistenciaPdfExporter.java
@@ -1,135 +1,126 @@
-// import java.io.IOException;
-// import java.time.LocalDate;
-// import java.time.ZoneId;
-// import java.time.format.DateTimeFormatter;
-// import java.util.Date;
-// import java.util.List;
-// import javax.servlet.http.HttpServletResponse;
-// import com.crud.CRUDSpring.model.Clase;
-// import com.crud.CRUDSpring.model.Profesor;
-// import com.lowagie.text.Document;
-// import com.lowagie.text.DocumentException;
-// import com.lowagie.text.PageSize;
-// import com.lowagie.text.Phrase;
-// import com.lowagie.text.pdf.PdfPCell;
-// import com.lowagie.text.pdf.PdfPTable;
-// import com.lowagie.text.pdf.PdfWriter;
-// import java.awt.Color;
-// import java.io.IOException;
-// import java.util.List;
+package com.crud.CRUDSpring.util;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 
-// import javax.servlet.http.HttpServletResponse;
+import com.crud.CRUDSpring.model.Asistencia;
+import com.crud.CRUDSpring.model.Clase;
+import com.crud.CRUDSpring.model.Profesor;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import java.awt.Color;
+import java.io.IOException;
+import java.util.List;
 
-// import com.lowagie.text.*;
-// import com.lowagie.text.pdf.*;
+import javax.servlet.http.HttpServletResponse;
 
-// public class ClasePdfExporter {
-// 	private List<Clase> listClases;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.*;
 
-// 	private void writeTableHeader(PdfPTable table) {
-// 		PdfPCell cell = new PdfPCell();
-// 		cell.setBackgroundColor(Color.BLUE);
-// 		cell.setPadding(6);
+public class AsistenciaPdfExporter {
+	private Clase clase;
+    private List<Asistencia> asistencias;
+    private List<Asistencia> faltas;
+    private List<Asistencia> todasLasFechas;
+    Profesor profesor;
+    
+    
+	private void writeTableHeader(PdfPTable table) {
+		PdfPCell cell = new PdfPCell();
+		cell.setBackgroundColor(Color.BLUE);
+		cell.setPadding(6);
 
-// 		Font font = FontFactory.getFont(FontFactory.HELVETICA);
-// 		font.setColor(Color.WHITE);
+		Font titulo = FontFactory.getFont(FontFactory.HELVETICA);
+		titulo.setColor(Color.WHITE);
 
-// 		cell.setPhrase(new Phrase("Nombre", font));
-// 		table.addCell(cell);
+		cell.setPhrase(new Phrase("Fecha", titulo));
+		table.addCell(cell);
 
-// 		cell.setPhrase(new Phrase("Profesores", font));
-// 		table.addCell(cell);
+		cell.setPhrase(new Phrase("Estado", titulo));
+		table.addCell(cell);
+	}
 
-// 		cell.setPhrase(new Phrase("Deporte", font));
-// 		table.addCell(cell);
+	private void writeTableData(PdfPTable table) {
+		for (Asistencia asistencia : todasLasFechas) {
+			table.addCell(asistencia.getFechaAsistencia().toString());
+			agregarAsistencia(table, asistencia);
+			
+		}
+	}
 
-// 		cell.setPhrase(new Phrase("Inicio", font));
-// 		table.addCell(cell);
+	public void agregarAsistencia(PdfPTable table, Asistencia asistencia) {
+		if(asistencias.contains(asistencia)){
+            table.addCell("Presente");
+        }else{
+            table.addCell("ausente");
+        }
+	}
 
-// 		cell.setPhrase(new Phrase("Fin", font));
-// 		table.addCell(cell);
-// 	}
+	private void writeTableColumnHeader(PdfPTable tablaClases, String nombreColumna) {
+		Font font = FontFactory.getFont(FontFactory.HELVETICA);
+		font.setColor(Color.WHITE);
+		PdfPCell celda = new PdfPCell();
+		celda.setBackgroundColor(Color.BLUE);
+		celda.setPadding(5);
 
-// 	private void writeTableData(PdfPTable table) {
-// 		for (Clase clase : listClases) {
-// 			table.addCell(clase.getNombreDep());
-// 			agregarProfesorTable(table, clase);
-// 			table.addCell(clase.getDeporte());
-// 			table.addCell(clase.getFechaInicio().toString());
-// 			table.addCell(clase.getFechaFin().toString());
-// 		}
-// 	}
+		Phrase frase = new Phrase(nombreColumna, font);
+		celda.setPhrase(frase);
+		tablaClases.addCell(celda);
 
-// 	public PdfPTable agregarProfesorTable(PdfPTable tablaClases, Clase clase) {
-// 		PdfPTable tablaProfesor = new PdfPTable(2);
-// 		tablaProfesor.setWidthPercentage(100f);
-// 		int indexProf = clase.getProfesores().size();
-// 		List<Profesor> listaProfesores = clase.getProfesores();
-// 		writeTableColumnHeader(tablaProfesor, "Nombre");
-// 		writeTableColumnHeader(tablaProfesor, "Apellido");
-// 		if (indexProf != 0) {
-// 			for (int i = 0; i < indexProf; i++) {
-// 				tablaProfesor.addCell(listaProfesores.get(i).getNombreProf());
-// 				tablaProfesor.addCell(listaProfesores.get(i).getApellidoProf());
-// 			}
-// 		} else {
-// 			tablaProfesor.addCell("-----");
-// 			tablaProfesor.addCell("-----");
-// 		}
-// 		tablaClases.addCell(tablaProfesor);
-// 		return tablaClases;
+	}
 
-// 	}
+	public void export(HttpServletResponse response) throws DocumentException, IOException {
+		Document document = new Document(PageSize.A4);
+		PdfWriter.getInstance(document, response.getOutputStream());
 
-// 	private void writeTableColumnHeader(PdfPTable tablaClases, String nombreColumna) {
-// 		Font font = FontFactory.getFont(FontFactory.HELVETICA);
-// 		font.setColor(Color.WHITE);
-// 		PdfPCell celda = new PdfPCell();
-// 		celda.setBackgroundColor(Color.BLUE);
-// 		celda.setPadding(5);
+		document.open();
+		Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+		font.setSize(18);
+		font.setColor(Color.BLUE);
+		Image deportesBaradero = Image.getInstance("src/main/resources/static/img/deportesPDF.jpg");
+		deportesBaradero.setWidthPercentage(50);
+		deportesBaradero.setAlignment(Paragraph.ALIGN_CENTER);
 
-// 		Phrase frase = new Phrase(nombreColumna, font);
-// 		celda.setPhrase(frase);
-// 		tablaClases.addCell(celda);
+		document.add(deportesBaradero);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy");
+		String date = new Date().toInstant().atZone(ZoneId.of("America/Argentina/Catamarca")).toLocalDate().format(formatter);
+		Paragraph p = new Paragraph("Lista de Clases - " + date.toString(), font);
+		p.setAlignment(Paragraph.ALIGN_CENTER);
 
-// 	}
+		document.add(p);
 
-// 	public void export(HttpServletResponse response) throws DocumentException, IOException {
-// 		Document document = new Document(PageSize.A4);
-// 		PdfWriter.getInstance(document, response.getOutputStream());
+		PdfPTable table = new PdfPTable(2);
+		table.setWidthPercentage(100f);
+		table.setWidths(new float[] { 2.0f, 2.0f });
+		table.setSpacingBefore(10);
 
-// 		document.open();
-// 		Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-// 		font.setSize(18);
-// 		font.setColor(Color.BLUE);
-// 		Image deportesBaradero = Image.getInstance("src/main/resources/static/img/deportesPDF.jpg");
-// 		deportesBaradero.setWidthPercentage(50);
-// 		deportesBaradero.setAlignment(Paragraph.ALIGN_CENTER);
+		writeTableHeader(table);
+		writeTableData(table);
 
-// 		document.add(deportesBaradero);
-// 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy");
-// 		String date = new Date().toInstant().atZone(ZoneId.of("America/Argentina/Catamarca")).toLocalDate().format(formatter);
-// 		Paragraph p = new Paragraph("Lista de Clases - " + date.toString(), font);
-// 		p.setAlignment(Paragraph.ALIGN_CENTER);
+		document.add(table);
 
-// 		document.add(p);
+		document.close();
 
-// 		PdfPTable table = new PdfPTable(5);
-// 		table.setWidthPercentage(100f);
-// 		table.setWidths(new float[] { 1.5f, 3.5f, 1.5f, 2.0f, 2.0f });
-// 		table.setSpacingBefore(10);
+	}
 
-// 		writeTableHeader(table);
-// 		writeTableData(table);
 
-// 		document.add(table);
+    
+    public AsistenciaPdfExporter(Profesor profesor, Clase clase, List<Asistencia> todasLasFechas, List<Asistencia> asistencias, List<Asistencia> faltas) {
+        this.clase = clase;
+        this.todasLasFechas = todasLasFechas;
+        this.asistencias = asistencias;
+        this.faltas = faltas;
+        this.profesor = profesor;
+    }
 
-// 		document.close();
-
-// 	}
-
-// 	public ClasePdfExporter(List<Clase> listClases) {
-// 		this.listClases = listClases;
-// 	}
-
-// }
+    
+}
